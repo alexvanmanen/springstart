9be2eeb8c2b803e03ef350fe3bedc13dc5feff8c
Alex <alexvanmanen@hotmail.com>
2021-11-30 18:02:30 +0100

    extra code

--- src/main/java/nl/springstart/practice/HelloController.java
+++ src/main/java/nl/springstart/practice/HelloController.java
@@ -1,18 +1,26 @@
 package nl.springstart.practice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    PersonRepository personRepository;
 
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
-    @GetMapping("/person/get")
-    public Person getPerson() {
-        return new Person(1L, "Alex", "van Manen");
+    @GetMapping("/person/get/{id}")
+    public Optional<Person> getPerson(@PathVariable int id) {
+        return personRepository.findById(id);
+        //return new Person(1L, "Alex", "van Manen");
     }
 }
--- src/main/resources/db/migration/V1_1__create_table_person.sql
+++ src/main/resources/db/migration/V1_1__create_table_person.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `person` (
     `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `firstName` varchar(20),
-    `lastName` varchar(20)
+    `first_name` varchar(20),
+    `last_name` varchar(20)
 );
---
--- INSERT INTO person (firstName, lastName) VALUES ( 'Stephen', 'Strange' );
--- INSERT INTO person (firstName, lastName) VALUES ( 'Peter', 'Parker' );
+
+INSERT INTO person (first_name, last_name) VALUES ( 'Stephen', 'Strange' );
+INSERT INTO person (first_name, last_name) VALUES ( 'Peter', 'Parker' );
